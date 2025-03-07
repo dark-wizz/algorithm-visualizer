3885e9061bf6835f56f621b32cf1a3e25126f4ef
dark-wizz <mailwithakram@gmail.com>
2025-03-07 17:58:20 +0530

    Implement player control buttons

--- src/comps/Player.jsx
+++ src/comps/Player.jsx
@@ -1,9 +1,22 @@
 import LinearProgress from '@mui/material/LinearProgress';
+import RestartAltOutlinedIcon from '@mui/icons-material/RestartAltOutlined';
+import SkipPreviousOutlinedIcon from '@mui/icons-material/SkipPreviousOutlined';
+import SkipNextOutlinedIcon from '@mui/icons-material/SkipNextOutlined';
+import AutoFixHighOutlinedIcon from '@mui/icons-material/AutoFixHighOutlined';
+import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
+import PauseCircleOutlineOutlinedIcon from '@mui/icons-material/PauseCircleOutlineOutlined';
+
 
 const Player = () => {
   return <div className="player">
     <LinearProgress variant="determinate" value={10} />
-    
+    <div className="player_control">
+      <RestartAltOutlinedIcon /> 
+      <SkipPreviousOutlinedIcon />
+      <PlayCircleFilledWhiteOutlinedIcon />
+      <SkipNextOutlinedIcon />
+      <AutoFixHighOutlinedIcon />
+    </div>
   </div>
 }
 

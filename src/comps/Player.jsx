376e79757e10a74654e9aa794a38a610a9c7e08f
dark-wizz <mailwithakram@gmail.com>
2025-03-09 17:15:22 +0530

    Fix player layout

--- src/comps/Player.jsx
+++ src/comps/Player.jsx
@@ -1,4 +1,4 @@
-import LinearProgress from '@mui/material/LinearProgress';
+import Slider from '@mui/material/Slider';
 import RestartAltOutlinedIcon from '@mui/icons-material/RestartAltOutlined';
 import SkipPreviousOutlinedIcon from '@mui/icons-material/SkipPreviousOutlined';
 import SkipNextOutlinedIcon from '@mui/icons-material/SkipNextOutlined';
@@ -6,15 +6,31 @@ import AutoFixHighOutlinedIcon from '@mui/icons-material/AutoFixHighOutlined';
 import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
 import PauseCircleOutlineOutlinedIcon from '@mui/icons-material/PauseCircleOutlineOutlined';
 import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import Input from '@mui/material/Input';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
 
 
 const Player = () => {
   return <div className="player">
-    <LinearProgress variant="determinate" value={10} />
-    <div className="player_control">
-      <Select 
-
-      />
+    <Slider />
+     <div className="player_control">
+      <FormControl sx={{ minWidth: 50 }} size="small">
+        <InputLabel id="player_speed_label">speed</InputLabel>
+        <Select
+          labelId="player_speed_label"
+          id="player_speed"
+          value={1}
+          label="speed"
+          autoWidth
+        >
+          <MenuItem value={0.5}>0.5x</MenuItem>
+          <MenuItem value={1}>1x</MenuItem>
+          <MenuItem value={1.5}>1.5x</MenuItem>
+          <MenuItem value={2}>2x</MenuItem>
+        </Select>
+      </FormControl>
       <div className="buttons">
         <RestartAltOutlinedIcon /> 
         <SkipPreviousOutlinedIcon />
@@ -22,6 +38,10 @@ const Player = () => {
         <SkipNextOutlinedIcon />
         <AutoFixHighOutlinedIcon />
       </div>
+      <FormControl sx={{ width: "5em" }} size="small">
+        <InputLabel id="player_speed_label">size</InputLabel>
+        <Input type="number" defaultValue={10}/>
+      </FormControl>
     </div>
   </div>
 }

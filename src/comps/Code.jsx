b8cf2278477c2f121b2a599356b7e969e4e4e70b
dark-wizz <mailwithakram@gmail.com>
2025-03-09 13:42:33 +0530

    Add psuedocode to the code component

--- src/comps/Code.jsx
+++ src/comps/Code.jsx
@@ -1,7 +1,25 @@
 
+const codes = [
+  "procedure bubbleSort(A : list of sortable items)",
+  "  n := length(A)",
+  "  repeat",
+  "      swapped := false",
+  "      for i := 1 to n - 1 inclusive do",
+  "          if A[i - 1] > A[i] then",
+  "              swap(A[i - 1], A[i])",
+  "              swapped := true",
+  "          end if",
+  "      end for",
+  "      n := n - 1",
+  "  until not swapped",
+  "end procedure",
+]
+
 const Code = () => {
   return <div className="code">
-    Code
+    {codes.map((v,i)=>{
+      return <p key={i}>{v}</p>
+    })}
   </div>
 }
 

f418783a6c91f87f761dfe5065ee85ef2682b952
dark-wizz <mailwithakram@gmail.com>
2025-03-21 01:19:13 +0530

    Add util x translate

--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -2,3 +2,9 @@ export const genRandomArr = (min, max, n) => {
   return Array.from({ length: n }, () => Math.floor(Math.random() * (max - min + 1)) + min);
 };
 
+export const xFromTranslate = (val = "") => {
+  let prop = val.match(/translatex\(-?\d+.*?\)/i)?.[0] ?? "0";
+  let x = prop?.match(/-?\d+/)[0] ?? 0;
+  return +x;
+};
+
